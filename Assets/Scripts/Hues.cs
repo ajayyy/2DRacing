e0206bfb2a126249870927772953b6e31822b6bf
thebestusernameever <ajay@ajayinkingston.com>
2017-11-14 08:19:10 -0500

    added Hues.cs back

---
+++ Assets/Scripts/Hues.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Hues : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float h;
+		float s;
+		float v;
+		Color.RGBToHSV (GetComponent<Text>().color, out h, out s, out v);
+		//adjust hue
+		Color adjustedColor = Color.HSVToRGB (h + 0.4f * Time.deltaTime, s, v);
+        GetComponent<Text>().color = adjustedColor;
+		
+	}
+}
